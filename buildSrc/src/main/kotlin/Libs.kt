15a82ef2a6eb248a6a38ca65b70d28bbf1948e89
sksamuel <sam@sksamuel.com>
2021-03-07 20:39:42 -0600

    Migrated project from main kotest repo

---
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -0,0 +1,21 @@
+object Libs {
+
+   const val kotlinVersion = "1.4.31"
+   const val org = "io.kotest"
+
+   object Kotlin {
+      const val stdlib = "org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion"
+   }
+
+   object Kotest {
+      private const val version = "4.4.1"
+      const val assertions = "io.kotest:kotest-assertions-core-jvm:$version"
+      const val api = "io.kotest:kotest-framework-api:$version"
+      const val junit5 = "io.kotest:kotest-runner-junit5-jvm:$version"
+   }
+
+   object Wiremock {
+      const val version = "2.27.2"
+      const val wiremock = "com.github.tomakehurst:wiremock:$version"
+   }
+}
